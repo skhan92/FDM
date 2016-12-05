6b3c0047583e9af821b74c8b01e2a1976c645d21
suleman.khan <suleman.khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-05 14:59:37 +0000

    Day 1 - Exercise 1

---
+++ Exercise1SyntaxPractise/Exercise1SyntaxPractise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+            if (i % 15 == 0)
+            {   
+                System.Console.WriteLine("Fizzbuzz");
+            }
+            else if (i % 5 == 0)
+            {    
+                System.Console.WriteLine("Buzz");
+            }
+            else if (i % 3 == 0)
+            {    
+                System.Console.WriteLine("Fizz");
+            } 
+            else
+            {
+                System.Console.WriteLine(i);
+            }
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
