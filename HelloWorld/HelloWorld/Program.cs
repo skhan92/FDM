7013d8fe67125e230c6186f2aa7047da90c252a3
suleman.khan <suleman.khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-05 11:00:44 +0000

    Initial version of HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine(userInput);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+//THIS IS A COMMENT//
+
