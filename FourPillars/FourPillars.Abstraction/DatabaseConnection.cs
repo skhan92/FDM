4f5c9d1a9255b50a3fe6fbfcdaef81190cbd842a
Suleman.Khan <Suleman.Khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-06 11:51:31 +0000

    Abstraction - 4 Pillars

---
+++ FourPillars/FourPillars.Abstraction/DatabaseConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourPillars.Abstraction
+{
+    abstract class DatabaseConnection
+    {
+        public abstract void ConnectToDatabase(string databaseAddress);
+    }
+
+    class OracleDatabaseConnection : DatabaseConnection
+    {
+        public override void ConnectToDatabase(string databaseAddress)
+        {
+            Console.WriteLine("Opening connection to Oracle Database");
+        }
+    }
+
+    class MicrosoftDatabaseConnection : DatabaseConnection
+    {
+        public override void ConnectToDatabase(string databaseAddress)
+        {
+            Console.WriteLine("Opening connection to Microsoft Database");
+        }
+    }
+}
