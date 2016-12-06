087ffa8b141f2c710e42a6bfeed8f10ee0197c84
Suleman.Khan <Suleman.Khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-06 11:12:32 +0000

    Encapsulation - examples and notes

--- FourPillars/FourPillars.Encapsulation/Program.cs
+++ FourPillars/FourPillars.Encapsulation/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace FourPillars.Encapsulation
 {
-    class Program
+    class Student
     {
-        static void Main(string[] args)
-        {
-        }
+        //ENCAPSULATION - grouping RELEVANT stuff together//
+
+        public string name { get; set; }
+        public int id { get; set; }
+        public string gender { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public decimal debt { get; set; }
+        //these below are not relevant for a student
+        //public int StudentUnionPhoneNumber { get; set; }
+        //public DateTime TermDates { get; set; }
+
+        //Variables encapsulate pieces of data
+        //Methods encapsulate logic
+        //Objects/classes encapsulate variables and methods
+        //Projects encapsulate classes
+        //Solutions encapsulate projects
     }
 }
