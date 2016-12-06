f55bace4995fe704d5f6cb01516eefece6835774
Suleman.Khan <Suleman.Khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-06 12:11:48 +0000

    Polymorphism - Four Pillars

---
+++ FourPillars/FourPillars.Polymorphism/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourPillars.Polymorphism
+{
+    class LoginController
+    {
+        public virtual void Login(string username)
+        {
+            Console.WriteLine("Logging in with username:" + username);
+        }
+        //This is Overloaded, w a different type of parameter
+        public void Login(int id)
+        {
+            Console.WriteLine("Logging in with id:" + id);
+        }
+        //Overloaded with an extra parameter
+        public void Login(string email, string password)
+        {
+            Console.WriteLine("Logging in with email:" + email + "and password:" + password);
+        }
+
+
+    }
+
+    class AdminLoginController : LoginController
+    {
+        public override void Login(string username)
+        {
+            Console.WriteLine("Logging in admin with username:" + username);
+        }
+    }
+}
---
+++ FourPillars/FourPillars.Polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourPillars.Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginController loginController = new LoginController();
+            loginController.Login(123);
+            loginController.Login("123");
+            loginController.Login("123@123.com", "123");
+
+            AdminLoginController adminLoginController = new AdminLoginController();
+            adminLoginController.Login("123");
+
+            Console.ReadLine();
+        }
+    }
+}
