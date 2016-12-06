dbd6a09dbb80e946dc72a2110865cc4fbe99314a
Suleman.Khan <Suleman.Khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-06 11:42:52 +0000

    Inheritance - Four Pillars

---
+++ FourPillars/FourPillars.Inheritance/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourPillars.Inheritance
+{
+    //This is an example of inheritance using concrete classes
+    class UserController
+    {
+        public void Login(string username, string password)
+        {
+            Console.WriteLine("Logging in -" + username);
+        }
+    }
+
+    class AdminController : UserController
+    {
+        public void SetPermissions(string username)
+        {
+            Console.WriteLine("Setting Permissions for -" + username);
+        }
+    }
+
+    class SuperAdminController : AdminController
+    {
+
+    }
+}
