6819387795d5a9377c85bbf481a15ba82d2eefa5
Suleman.Khan <Suleman.Khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-06 16:49:36 +0000

    Task 3 Completed

---
+++ Exercise1SyntaxPractise/Task 3/View.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class View
+    {
+        public void PrintEligible(Person person)
+        {
+            Console.WriteLine(string.Format("{0} {1} may qualify for a pension this year", person.firstName, person.lastName));
+        }
+
+        public void PrintIneligible(Person person)
+        {
+            Console.WriteLine(string.Format("{0} {1} may not qualify for a pension this year", person.firstName, person.lastName));
+        }
+    }
+}
