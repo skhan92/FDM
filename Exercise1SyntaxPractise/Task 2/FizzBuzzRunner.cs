b2029734790d64aca30b2c24af6ea1a8880632df
Suleman.Khan <Suleman.Khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-06 15:20:21 +0000

    Task 2 Complete

---
+++ Exercise1SyntaxPractise/Task 2/FizzBuzzRunner.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class FizzBuzzRunner
+{
+        public void FizzBuzz(int num) {
+            for (int i = 1; i <= num; i++)
+            {
+                if (IsFizzBuzz(i) == true)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                }
+                
+                else if (IsBuzz(i) == true)
+                {
+                    System.Console.WriteLine("Buzz");
+                }
+
+                else if (IsFizz(i) == true)
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+                
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+
+        private bool IsFizz(int num)
+            {
+            if (num % 3 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            }
+
+        private bool IsBuzz (int num)
+            {
+            if (num % 5 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            }
+
+        private bool IsFizzBuzz (int num)
+            {
+            if (num % 15 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            }
+    
+    }
+}
---
+++ Exercise1SyntaxPractise/Task 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzRunner runner = new FizzBuzzRunner();
+            runner.FizzBuzz(100);
+            System.Console.ReadLine();
+        }
+    }
+}
+
+
