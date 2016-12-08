38eb31af43f0817cf4bf21ac246dec647a7f9bc4
suleman.khan <suleman.khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-08 15:48:42 +0000

    Presentation Code

---
+++ Presentation Test/Presentation Test/MathsExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation_Test
+{
+        class MainClass
+        {
+        static void Main()
+        {
+            double dub = 8.34;
+           
+            Console.WriteLine(Math.Abs(dub));
+            Console.WriteLine(Math.Floor(dub)); 
+            Console.WriteLine(Math.Round(Math.Abs(dub)));
+
+           
+            Console.ReadLine();
+        }
+        }
+        }
+
