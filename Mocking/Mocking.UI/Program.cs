ac4ed11a00dcaf2f550342bc0a8470ffb0ddd202
suleman.khan <suleman.khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-08 12:13:42 +0000

    Class Examples - TDD Advanced

---
+++ Mocking/Mocking.UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Mocking.BookShop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mocking.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseReader databaseReader = new DatabaseReader();
+            StockChecker stockChecker = new StockChecker(databaseReader);
+
+            int stock = stockChecker.NumberInStock("ABC123");
+
+            Console.WriteLine("Number in stock: " + stock);
+            Console.ReadLine();
+        }
+    }
+}
