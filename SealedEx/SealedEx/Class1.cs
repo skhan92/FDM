495efef59e3cf72ccfb2c0e7fe1029ea573cb51d
suleman.khan <suleman.khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-09 08:55:19 +0000

    Sealed Example

---
+++ SealedEx/SealedEx/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedEx
+{
+    class SealedTest2
+    {
+        static void Main()
+        {
+            SealedClass sc = new SealedClass();
+            sc.x = 110;
+            sc.y = 150;
+            Console.WriteLine("a = {0}, a = {1}", sc.x, sc.y);
+            Console.ReadLine();
+        }
+    }
+}
