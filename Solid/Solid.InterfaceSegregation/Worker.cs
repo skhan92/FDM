97e732e9c50f9223d3575a331a48c742fc3cc486
suleman.khan <suleman.khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-13 12:10:59 +0000

    SOLID EXAMPLES

---
+++ Solid/Solid.InterfaceSegregation/Worker.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.InterfaceSegregation
+{
+    //public interface Worker //Interface is essentially abstract so it helps w dependency inverison and the other principles
+    //{
+    //    void Work();
+    //    void Eat();
+    //}
+
+    //BELOW IS INTERFACE SEGREGATION
+    public interface Worker
+    {
+        void Work();
+    }
+   
+    public interface Eater
+    {
+        void Eat();
+    }
+
+
+    public class EfficientWorker : Worker, Eater
+    {
+
+        public void Work()
+        {
+            Console.WriteLine("I work as much as I can");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("I only eat when I have to");
+        }
+    }
+
+    public class LazyWorker : Worker, Eater
+    {
+
+        public void Work()
+        {
+            Console.WriteLine("I only work when I am being watched");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("I eat whenever I feel like it");
+        }
+    }
+
+    public class RobotWorker : Worker //Issue: doesn't use the same functions as workers, no need to eat..
+    {
+        public void Work()
+        {
+            Console.WriteLine("I work 24/7");
+        }
+
+        //BELOW IS INTERFACE POLLUTION, USED INTERFACE SEG TO DEAL W THIS
+        //public void Eat()
+        //{
+        //    Console.WriteLine("I don't eat");
+        //}
+    }
+}
---
+++ Solid/Solid.OpenCloseDependencyInversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.OpenCloseDependencyInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Best way of making it OpenClose is Dependency Inversion
+            //i.e. If you do dependency inversion, very likely, openclose will take care of itself
+
+            TradeController tradeController = new TradeController();
+            tradeController.TradeBonds(new Bond());
+            tradeController.TradeShare(new Share());
+
+            DependencyInversionTradeController dptc = new DependencyInversionTradeController();
+            dptc.Trade(new Bond());
+            dptc.Trade(new ForeignEx());
+            dptc.Trade(new Share());
+        }
+    }
+}
