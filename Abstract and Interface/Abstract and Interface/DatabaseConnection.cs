ed68d8f0720cad8a016845c23598ca68e72e7d5e
suleman.khan <suleman.khan@c5bca92a-6c41-4b79-b353-003551752103>
2016-12-15 15:55:27 +0000

    Abstract & Interface

---
+++ Abstract and Interface/Abstract and Interface/DatabaseConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_and_Interface
+{
+    interface DatabaseConnection
+    {
+        bool OpenConnectionToDatabase(string databaseAddress);
+    }
+
+    class MicrosoftDatabaseConnection : DatabaseConnection
+    {
+
+        public bool OpenConnectionToDatabase(string databaseAddress)
+        {
+            return true;
+        }
+    }
+
+    class OracleDatabaseConnection : DatabaseConnection
+    {
+
+        public bool OpenConnectionToDatabase(string databaseAddress)
+        {
+            return false;
+        }
+    }
+}
